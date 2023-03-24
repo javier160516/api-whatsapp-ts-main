ded3275d0211b83d7234f9eef089509c13f96053
javier160516 <javiercambranis23@gmail.com>
2023-03-24 10:27:05 -0500

    .

---
+++ src/infrastructure/router/lead.route.ts
@@ -0,0 +1,13 @@
+import express, { Router } from "express";
+import LeadCtrl from "../controller/lead.ctrl";
+import container from "../ioc";
+const router: Router = Router();
+
+/**
+ * http://localhost/lead POST
+ */
+const leadCtrl: LeadCtrl = container.get("lead.ctrl");
+router.post("/", leadCtrl.sendCtrl);
+router.get("/getqr", leadCtrl.sendQrCtrl);
+
+export { router };
